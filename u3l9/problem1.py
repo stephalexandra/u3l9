fab80d99f228aeeea0c4907e90311936c2ccdd07
stephalexandra <44372966+stephalexandra@users.noreply.github.com>
2019-01-17 02:03:00 -0500

    Add files via upload

---
+++ u3l9/problem1.py
@@ -0,0 +1,20 @@
+
+print('-' * 60)
+
+print()
+
+print('Locker Bot: ')
+
+password = input('What is the password? ')
+
+if password == 'Open sesame':
+	print('Password is valid!')
+	print('Locker opening...')
+
+else: 
+	print('Password is invalid...enabling laser defense system.')
+	print('Pew Pew Pew')
+	print('Pew Pew Pew')
+	
+
+print('-' * 60)
---
+++ u3l9/problem2.py
@@ -0,0 +1,9 @@
+print('-'*60)
+print('Alien Greeter Bot: ')
+print()
+planet = input('What planet are you from? ')
+if planet == 'Earth' or 'earth':
+	print('Dammmmnnnnn, youre so fried, I thought you were an alien. Anyways ... have a good day!')
+else:
+	print('Welcome to Earth, please dont kill us!')
+print('-'*60)
